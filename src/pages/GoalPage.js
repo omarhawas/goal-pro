5e2891aa8286fbbf74f601e33064004dac981660
Omar Hawas <omarhawas94@gmail.com>
2022-09-04 15:30:47 -0400

    added calendar

--- src/pages/GoalPage.js
+++ src/pages/GoalPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Calendar, globalizeLocalizer } from "react-big-calendar";
+import globalize from "globalize";
 
-const GoalPage = () => {
+const localizer = globalizeLocalizer(globalize);
+
+const GoalPage = (props) => {
   return (
     <div>
       <p>GOAL PAGE</p>
+      <Calendar
+        localizer={localizer}
+        // events={myEventsList}
+        // startAccessor="start"
+        // endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 };
